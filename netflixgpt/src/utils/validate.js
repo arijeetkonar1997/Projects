c9d8fc9259e53120d7b81da2ed4e2fd4e29acc83
Arijeet Konar <43075962+arijeetkonar1997@users.noreply.github.com>
2023-12-06 10:22:48 +0530

    Email and Password Validation in Form

---
+++ netflixgpt/src/utils/validate.js
@@ -0,0 +1,17 @@
+const validateCredentials = (email,password) => {
+    const checkName =/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+    const checkPassword =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    let errorMessage = "";
+
+    if(!checkName)
+    errorMessage = "Email not valid";
+
+    else if(!checkPassword)
+    errorMessage = "Password not valid";
+
+    else errorMessage = null;
+
+    return errorMessage
+}
+
+export default validateCredentials;
